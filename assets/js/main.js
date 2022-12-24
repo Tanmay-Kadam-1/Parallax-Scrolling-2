aa6848f28ff947bca5584c6bef90480cac50dc14
Tanmay Kadam <justcoding1482@gmail.com>
2022-12-24 11:56:22 +0530

    Completed Website with Animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,36 @@ const scrollActive = () => {
 window.addEventListener("scroll", scrollActive);
 
 /*=============== PARALLAX ===============*/
+let parallax = new Rellax(".parallax");
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from(".home__village", 1.2, { opacity: 0, y: 100, delay: 0.1 });
+gsap.from(".home__pine", 1.2, { opacity: 0, y: 150, delay: 0.3 });
+gsap.from(".home__mountain-2", 1.2, { opacity: 0, x: 150, delay: 0.5 });
+gsap.from(".home__mountain-3", 1.2, { opacity: 0, x: -150, delay: 0.6 });
+gsap.from(".home__mountain-1", 1.2, { opacity: 0, y: 250, delay: 0.7 });
+gsap.from(".home__moon", 1.2, { opacity: 0, y: 200, delay: 0.8 });
+gsap.from(".home__trineo", 1.2, { opacity: 0, x: -200, delay: 0.8 });
+gsap.from(".home__title", 1.2, { opacity: 0, y: -60, delay: 1 });
+gsap.from(
+	".send__snow-1, .send__snow-2,.send__snow-3, .celebrate__snow-1, .celebrate__snow-2",
+	1.2,
+	{
+		opacity: 0,
+		y: 60,
+		delay: 3,
+	}
+);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+	origin: "top",
+	distance: "60px",
+	duration: 2500,
+	delay: 400,
+});
+
+sr.reveal(".about__data, .celebrate__img", { origin: "right" });
+sr.reveal(".about__img, .celebrate__data", { origin: "left" });
+sr.reveal(".footer", { origin: "bottom", interval: 200 });
+sr.reveal(".send__card", { interval: 100 });
